28ad5ecdd4d9cd1af262e9c7db2bf9908d0a410d
Daniel Ellermann <d.ellermann@amc-world.de>
2011-06-19 10:55:09 +0000

    - Added tax value to tax class.

--- grails-app/domain/org/amcworld/springcrm/TaxClass.groovy
+++ grails-app/domain/org/amcworld/springcrm/TaxClass.groovy
@@ -1,4 +1,10 @@
 package org.amcworld.springcrm
 
 class TaxClass extends SelValue {
+	
+	static constraints = {
+		taxValue(scale:2, min:0.0d)
+	}
+	
+	double taxValue
 }
--- test/unit/org/amcworld/springcrm/TaxClassTests.groovy
+++ test/unit/org/amcworld/springcrm/TaxClassTests.groovy
@@ -13,8 +13,20 @@ class TaxClassTests extends GrailsUnitTestCase {
     }
 
     void testConstruct() {
-        TaxClass tc = new TaxClass(name:"19%", orderId:80)
+        TaxClass tc = new TaxClass(name:"19%", orderId:80, taxValue:19)
         assertEquals "19%", tc.name
         assertEquals 80, tc.orderId
+		assertEquals 19, tc.taxValue
     }
+	
+	void testMinConstraints() {
+		mockForConstraintsTests TaxClass
+		def validationFields = ["taxValue"]
+		TaxClass tc = new TaxClass(taxValue:-0.5)
+		assertFalse tc.validate(validationFields)
+		assertEquals "min", tc.errors["taxValue"]
+		
+		tc = new TaxClass(taxValue:0)
+		assertTrue tc.validate(validationFields)
+	}
 }
