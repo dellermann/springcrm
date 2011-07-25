bfab068b56896688f99cc837a9745997710f8271
Daniel Ellermann <d.ellermann@amc-world.de>
2011-07-25 18:42:19 +0000

    - Added post-import operations to remove duplicate data and fix   malformed values

--- scripts/db/post-convert.sql
+++ scripts/db/post-convert.sql
@@ -1,18 +1,79 @@
 --
+-- remove duplicated person data
+UPDATE person
+  SET other_addr_country = '',
+    other_addr_location = '',
+    other_addr_po_box = '',
+    other_addr_postal_code = '',
+    other_addr_state = '',
+    other_addr_street = ''
+  WHERE TRIM(mailing_addr_country) = TRIM(other_addr_country)
+    AND TRIM(mailing_addr_location) = TRIM(other_addr_location)
+    AND TRIM(mailing_addr_po_box) = TRIM(other_addr_po_box)
+    AND TRIM(mailing_addr_postal_code) = TRIM(other_addr_postal_code)
+    AND TRIM(mailing_addr_state) = TRIM(other_addr_state)
+    AND TRIM(mailing_addr_street) = TRIM(other_addr_street);
+UPDATE person
+  SET phone_other = ''
+  WHERE TRIM(phone) = TRIM(phone_other);
+  
+--
+-- fix abbreviations and malformed data in organization and person records
+UPDATE organization
+  SET billing_addr_street = REPLACE(
+      REPLACE(billing_addr_street, 'str.', 'straße'),
+      'Str.', 'Straße'
+    ),
+    shipping_addr_street = REPLACE(
+      REPLACE(shipping_addr_street, 'str.', 'straße'),
+      'Str.', 'Straße'
+    );
+UPDATE person
+  SET mailing_addr_street = REPLACE(
+      REPLACE(mailing_addr_street, 'str.', 'straße'),
+      'Str.', 'Straße'
+    ),
+    other_addr_street = REPLACE(
+      REPLACE(other_addr_street, 'str.', 'straße'),
+      'Str.', 'Straße'
+    );
+UPDATE organization
+  SET billing_addr_street = LEFT(
+      billing_addr_street, CHAR_LENGTH(billing_addr_street) - 1
+    )
+  WHERE RIGHT(billing_addr_street, 1) = ',';
+UPDATE organization
+  SET shipping_addr_street = LEFT(
+      shipping_addr_street, CHAR_LENGTH(shipping_addr_street) - 1
+    )
+  WHERE RIGHT(shipping_addr_street, 1) = ',';
+UPDATE organization
+  SET website = CONCAT('http://', website)
+  WHERE website <> ''
+    AND website NOT RLIKE '^https?://';
+UPDATE person
+  SET mailing_addr_street = LEFT(
+      mailing_addr_street, CHAR_LENGTH(mailing_addr_street) - 1
+    )
+  WHERE RIGHT(mailing_addr_street, 1) = ',';
+UPDATE person
+  SET other_addr_street = LEFT(
+      other_addr_street, CHAR_LENGTH(other_addr_street) - 1
+    )
+  WHERE RIGHT(other_addr_street, 1) = ',';
+
+--
 -- fix selector values and invalid entries
 UPDATE service
-  SET
-    category_id = category_id + 1999,
+  SET category_id = category_id + 1999,
     unit_id = unit_id + 299;
 
 UPDATE product
-  SET
-    category_id = category_id + 2999,
+  SET category_id = category_id + 2999,
     unit_id = unit_id + 299;
 
 UPDATE invoicing_transaction
-  SET
-    carrier_id = carrier_id + 499,
+  SET carrier_id = carrier_id + 499,
     quote_stage_id = quote_stage_id + 599,
     so_stage_id = so_stage_id + 799,
     invoice_stage_id = invoice_stage_id + 899
