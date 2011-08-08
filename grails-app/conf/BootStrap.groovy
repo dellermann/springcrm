92743847f7489b5e85d5c4f59ebd6ac1f924b1ff
Daniel Ellermann <d.ellermann@amc-world.de>
2011-08-08 15:20:24 +0000

    - Bugfix: broken pipe exception in data source.

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,8 +1,11 @@
-import org.amcworld.springcrm.ConfigHolder;
+import org.amcworld.springcrm.ConfigHolder
+import org.amcworld.springcrm.util.DataSourceUtils
 
 class BootStrap {
 
     def init = { servletContext ->
+		DataSourceUtils.tune(servletContext)
+
 		def configHolder = new ConfigHolder()
 		servletContext.config = configHolder
     }
