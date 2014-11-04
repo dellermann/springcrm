28d818f349216acae93b408fe3e84809151a1340
Daniel Ellermann <d.ellermann@amc-world.de>
2014-11-04 13:21:28 +0100

    Fix NPE
    I fix an NPE which appeared in a functional test.

--- grails-app/taglib/org/amcworld/springcrm/PermissionTagLib.groovy
+++ grails-app/taglib/org/amcworld/springcrm/PermissionTagLib.groovy
@@ -1,7 +1,7 @@
 /*
  * PermissionTagLib.groovy
  *
- * Copyright (c) 2011-2013, Daniel Ellermann
+ * Copyright (c) 2011-2014, Daniel Ellermann
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -70,7 +70,7 @@ class PermissionTagLib {
         } else if (!(controllers instanceof List)) {
             controllers = [controllers]
         }
-        if (session.user.checkAllowedControllers(controllers)) {
+        if (session.user?.checkAllowedControllers(controllers)) {
             out << body()
         }
     }
