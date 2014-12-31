9a452217c4730d56a0b4703916f81ef3b7d1a7fe
Daniel Ellermann <d.ellermann@amc-world.de>
2014-12-31 18:03:29 +0100

    Organization list
    I rework the organization list with the new design.

--- grails-app/taglib/org/amcworld/springcrm/ViewTagLib.groovy
+++ grails-app/taglib/org/amcworld/springcrm/ViewTagLib.groovy
@@ -22,7 +22,9 @@ package org.amcworld.springcrm
 
 import grails.util.GrailsNameUtils
 import java.util.regex.Pattern
+import org.codehaus.groovy.grails.web.mapping.UrlMapping
 import org.springframework.validation.FieldError
+import org.springframework.web.servlet.support.RequestContextUtils as RCU
 
 
 /**
@@ -568,6 +570,175 @@ class ViewTagLib {
     }
 
     /**
+     * Creates next/previous links to support pagination for the current controller.<br/>
+     *
+     * &lt;g:paginate total="${Account.count()}" /&gt;<br/>
+     *
+     * @emptyTag
+     *
+     * @attr total REQUIRED The total number of results to paginate
+     * @attr action the name of the action to use in the link, if not specified the default action will be linked
+     * @attr controller the name of the controller to use in the link, if not specified the current controller will be linked
+     * @attr id The id to use in the link
+     * @attr params A map containing request parameters
+     * @attr prev The text to display for the previous link (defaults to "Previous" as defined by default.paginate.prev property in I18n messages.properties)
+     * @attr next The text to display for the next link (defaults to "Next" as defined by default.paginate.next property in I18n messages.properties)
+     * @attr omitPrev Whether to not show the previous link (if set to true, the previous link will not be shown)
+     * @attr omitNext Whether to not show the next link (if set to true, the next link will not be shown)
+     * @attr omitFirst Whether to not show the first link (if set to true, the first link will not be shown)
+     * @attr omitLast Whether to not show the last link (if set to true, the last link will not be shown)
+     * @attr max The number of records displayed per page (defaults to 10). Used ONLY if params.max is empty
+     * @attr maxsteps The number of steps displayed for pagination (defaults to 10). Used ONLY if params.maxsteps is empty
+     * @attr offset Used only if params.offset is empty
+     * @attr mapping The named URL mapping to use to rewrite the link
+     * @attr fragment The link fragment (often called anchor tag) to use
+     */
+    Closure paginate = { attrs ->
+        def writer = out
+        if (attrs.total == null) {
+            throwTagError("Tag [paginate] is missing required attribute [total]")
+        }
+
+        def messageSource = grailsAttributes.messageSource
+        def locale = RCU.getLocale(request)
+
+        def total = attrs.int('total') ?: 0
+        def offset = params.int('offset') ?: 0
+        def max = params.int('max')
+        def maxsteps = (attrs.int('maxsteps') ?: 10)
+
+        if (!offset) offset = (attrs.int('offset') ?: 0)
+        if (!max) max = (attrs.int('max') ?: 10)
+
+        def linkParams = [:]
+        if (attrs.params) linkParams.putAll(attrs.params)
+        linkParams.offset = offset - max
+        linkParams.max = max
+        if (params.sort) linkParams.sort = params.sort
+        if (params.order) linkParams.order = params.order
+
+        def linkTagAttrs = [:]
+        def action
+        if (attrs.containsKey('mapping')) {
+            linkTagAttrs.mapping = attrs.mapping
+            action = attrs.action
+        } else {
+            action = attrs.action ?: params.action
+        }
+        if (action) {
+            linkTagAttrs.action = action
+        }
+        if (attrs.controller) {
+            linkTagAttrs.controller = attrs.controller
+        }
+        if (attrs.containsKey(UrlMapping.PLUGIN)) {
+            linkTagAttrs.put(UrlMapping.PLUGIN, attrs.get(UrlMapping.PLUGIN))
+        }
+        if (attrs.containsKey(UrlMapping.NAMESPACE)) {
+            linkTagAttrs.put(UrlMapping.NAMESPACE, attrs.get(UrlMapping.NAMESPACE))
+        }
+        if (attrs.id != null) {
+            linkTagAttrs.id = attrs.id
+        }
+        if (attrs.fragment != null) {
+            linkTagAttrs.fragment = attrs.fragment
+        }
+        linkTagAttrs.params = linkParams
+
+        // determine paging variables
+        def steps = maxsteps > 0
+        int currentstep = (offset / max) + 1
+        int firststep = 1
+        int laststep = Math.round(Math.ceil(total / max))
+
+        writer << '<ul class="pagination">'
+
+        // display previous link when not on firststep unless omitPrev is true
+        if (currentstep > firststep && !attrs.boolean('omitPrev')) {
+            linkParams.offset = offset - max
+            writer << '<li>' << link(linkTagAttrs.clone()) {
+                StringBuilder buf = new StringBuilder('<span aria-hidden="true">')
+                buf << (attrs.prev ?: messageSource.getMessage('default.paginate.prev.short', null, '«', locale))
+                buf << '</span>'
+                buf << '<span class="sr-only">'
+                buf << messageSource.getMessage('default.paginate.prev', null, 'Previous', locale)
+                buf << '</span>'
+            } << '</li>'
+        }
+
+        // display steps when steps are enabled and laststep is not firststep
+        if (steps && laststep > firststep) {
+
+            // determine begin and endstep paging variables
+            int beginstep = currentstep - Math.round(maxsteps / 2) + (maxsteps % 2)
+            int endstep = currentstep + Math.round(maxsteps / 2) - 1
+
+            if (beginstep < firststep) {
+                beginstep = firststep
+                endstep = maxsteps
+            }
+            if (endstep > laststep) {
+                beginstep = laststep - maxsteps + 1
+                if (beginstep < firststep) {
+                    beginstep = firststep
+                }
+                endstep = laststep
+            }
+
+            // display firststep link when beginstep is not firststep
+            if (beginstep > firststep && !attrs.boolean('omitFirst')) {
+                linkParams.offset = 0
+                writer << '<li>' << link(linkTagAttrs.clone()) {firststep.toString()} << '</li>'
+            }
+            //show a gap if beginstep isn't immediately after firststep, and if were not omitting first or rev
+            if (beginstep > firststep+1 && (!attrs.boolean('omitFirst') || !attrs.boolean('omitPrev')) ) {
+                writer << '<li class="disabled"><a href="#">…</a></li>'
+            }
+
+            // display paginate steps
+            (beginstep..endstep).each { i ->
+                if (currentstep == i) {
+                    writer << """\
+<li class="active">
+  <span>${i} <span class="sr-only">${messageSource.getMessage('default.paginate.current', null, 'current', locale)}</span></span>
+</li>
+"""
+                }
+                else {
+                    linkParams.offset = (i - 1) * max
+                    writer << '<li>' << link(linkTagAttrs.clone()) {i.toString()} << '</li>'
+                }
+            }
+
+            //show a gap if beginstep isn't immediately before firststep, and if were not omitting first or rev
+            if (endstep+1 < laststep && (!attrs.boolean('omitLast') || !attrs.boolean('omitNext'))) {
+                writer << '<li class="disabled"><a href="#">…</a></li>'
+            }
+            // display laststep link when endstep is not laststep
+            if (endstep < laststep && !attrs.boolean('omitLast')) {
+                linkParams.offset = (laststep - 1) * max
+                writer << '<li>' << link(linkTagAttrs.clone()) { laststep.toString() } << '</li>'
+            }
+        }
+
+        // display next link when not on laststep unless omitNext is true
+        if (currentstep < laststep && !attrs.boolean('omitNext')) {
+            linkTagAttrs.class = 'nextLink'
+            linkParams.offset = offset + max
+            writer << '<li>' << link(linkTagAttrs.clone()) {
+                StringBuilder buf = new StringBuilder('<span aria-hidden="true">')
+                buf << (attrs.prev ?: messageSource.getMessage('default.paginate.next.short', null, '»', locale))
+                buf << '</span>'
+                buf << '<span class="sr-only">'
+                buf << messageSource.getMessage('default.paginate.next', null, 'Next', locale)
+                buf << '</span>'
+            } << '</li>'
+        }
+
+        writer << '</ul>'
+    }
+
+    /**
      * Renders the errors of child items of the given bean.  If you specify a
      * body it may be used to output the error messages using either the
      * variable in attribute <code>var</code> or <code>it</code>.  If you omit
@@ -757,6 +928,10 @@ class ViewTagLib {
 
 
     //-- Non-public methods ---------------------
+
+    private callLink(Map attrs, Object body) {
+        TagOutput.captureTagOutput(tagLibraryLookup, 'g', 'link', attrs, body, webRequest)
+    }
 
     /**
      * Gets the currently active currency.
