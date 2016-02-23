924d0db1f13e12d6f7030fa7231db25d9241c8da
Daniel Ellermann <d.ellermann@amc-world.de>
2016-02-23 19:26:21 +0100

    Issue #5: lonely "next" button in list view
    Fix the issue that a lonely "next" button appears on lists with only one
    item.  It was caused by a improperly computed offset in ViewFilters
    class.

--- grails-app/conf/org/amcworld/springcrm/ViewFilters.groovy
+++ grails-app/conf/org/amcworld/springcrm/ViewFilters.groovy
@@ -91,7 +91,7 @@ class ViewFilters {
 
                 int count = cls.clazz.'count'()
                 int max = Math.min(params.max ? params.int('max') : 10, 100)
-                int maxOffset = Math.floor((count - 1) / max) * max
+                int maxOffset = count > 0 ? Math.floor((count - 1) / max) * max : 0
                 params.offset = Math.min(maxOffset, params.int('offset') ?: 0)
                 session[key] = params.offset
 
